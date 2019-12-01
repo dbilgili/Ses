f2655a390f46b9d80af04e6f1461a188df039689
Dogacan Bilgili <dbilgili@sabanciuniv.edu>
2019-12-01 18:47:28 +0100

    Remove branch content

--- src/client/shared/utils/electron/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-const { ipcRenderer, shell } = window.require('electron');
-
-export { default as SystemMenu } from './SystemMenu';
-export { ipcRenderer };
-export { shell };
